6d07301d4e601994cbb2ed0ca06b03a6a5ef5f8e
ixzez <159862817+KurbatovK@users.noreply.github.com>
2025-05-29 20:28:51 +0800

    Add files via upload

---
+++ TelegramBot-main/TelegramBot-main/TG BOT/config.py
@@ -0,0 +1,5 @@
+class Config:
+    TOKEN = '7717177353:AAHXXOVveceZPi4rMSsfMmzMm5GJe71CMo8'
+    API_URL = 'https://raspmath.isu.ru/getScheduleForTest'
+    API_TOKEN = 'da9ca528-5602-4f20-8d74-da2a73fe8cb3'
+    CACHE_TTL = 3600
